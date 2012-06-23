6a25cccd6a3cfe4c1e87e2688d95122294a58333
manuel.mundorf@gmail.com <manuel.mundorf@gmail.com@af280751-7691-d867-05e7-a1f7faff8bc0>
2012-06-23 15:30:06 +0000

    Add new level, delete unused imports

--- trunk/Pacman/src/server/LevelGenerator.java
+++ trunk/Pacman/src/server/LevelGenerator.java
@@ -14,16 +14,17 @@ public class LevelGenerator {
 	 * Returns a new generated level
 	 */
 	public static Level GetLevel(){
-		byte[][] temp = new byte[20][20];
+		byte[][] temp = null;
 		Random randomGenerator = new Random();
-		int lev = randomGenerator.nextInt(2);
+		int lev = randomGenerator.nextInt(3);
 		int cointSum = 1;
 		
 		switch(lev){
 			case 0:
+				temp = new byte[21][20];
 				for (int i = 0; i < temp.length; i++) {
 					for (int j = 0; j < temp[0].length; j++) {
-						if(0 == i || 19 == i){
+						if(0 == i || 20 == i){
 							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
 						} else if( 0 == j || 19 == j){
 							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
@@ -33,14 +34,15 @@ public class LevelGenerator {
 					}
 				}
 				
-				cointSum = 321;
+				cointSum = 339;
 				break;
 			case 1:
+				temp = new byte[21][21];
 				for (int i = 0; i < temp.length; i++) {
 					for (int j = 0; j < temp[0].length; j++) {
-						if(0 == i || 19 == i){
+						if(0 == i || 20 == i){
 							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
-						} else if( 0 == j || 19 == j){
+						} else if( 0 == j || 20 == j){
 							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
 						} else if (i % 2 == 0 && j % 2 == 0){
 							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
@@ -50,9 +52,26 @@ public class LevelGenerator {
 					}
 				}
 				
-				cointSum = 241;
+				cointSum = 277;
 				break;
-			
+			case 2:
+				temp = new byte[21][20];
+				for (int i = 0; i < temp.length; i++) {
+					for (int j = 0; j < temp[0].length; j++) {
+						if(0 == i || 20 == i){
+							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
+						} else if( 0 == j || 19 == j){
+							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
+						} else if (i % 2 == 0 && j != 1 && j != 18 && j != 10){
+							temp[i][j] = FieldState.getByteValue(FieldState.Wall);
+						} else {
+							temp[i][j] = FieldState.getByteValue(FieldState.Coin);
+						}
+					}
+				}
+				
+				cointSum = 204;
+				break;
 		}
 		
 		Level level = new Level(temp.length, temp[0].length);
